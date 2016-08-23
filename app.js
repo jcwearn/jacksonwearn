504813a7b699678bdd36708b439a2d6705acecc5
Jackson Wearn <jacksonwearn@gmail.com>
2016-08-23 16:36:03 -0400

    Remove jade

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var routes = require('./routes/index');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
