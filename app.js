d7313006d8bba3605021d33d4d89e2c0f62afb6d
Jackson Wearn <jacksonwearn@gmail.com>
2016-08-24 16:41:18 -0400

    Add body parser for json post route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
