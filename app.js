45b92ab1685ddbf78aa5b822913251290d649880
Jackson Wearn <jacksonwearn@gmail.com>
2017-05-01 23:33:17 -0400

    Add back in error handler and favicon.  Stuff for cookie parsing as well

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -9,11 +11,32 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.json());
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  var errorPage = path.join(__dirname, "../", "views/error.html");
+  res.sendFile(errorPage);
+});
 
 module.exports = app;
