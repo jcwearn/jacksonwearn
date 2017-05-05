be31886851562fa3253bac659dd38bc5f6482f36
Jackson Wearn <jacksonwearn@gmail.com>
2017-05-04 22:21:28 -0400

    Add scanForConnections.  Do things

--- public/javascripts/connect-four.js
+++ public/javascripts/connect-four.js
@@ -3,20 +3,53 @@ const PURPLE = 1;
 const GREEN = 2;
 
 function Game() {
-  this.board = new Board();
+  this.board = initBoard();
   this.playerTurn = PURPLE;
   this.turnCount = 0;
 };
 
-Game.takeTurn = {
+Game.prototype.takeTurn = function(colSelection) {
+  this.board = updateBoard(this.board, colSelection, this.playerTurn);
+  if (this.playerTurn === PURPLE)
+    this.playerTurn = GREEN;
+  else
+    this.playerTurn = PURPLE;
 
+  this.turnCount++;
 };
 
-function Space() {
-  this.state = EMPTY;
+Game.prototype.scanForConnections = function() {
+  let numRows = this.board[0].length;
+  for (let i; i < numRows; i++) {
+    let row = getRow(this.board, i);
+
+  }
+};
+
+function updateBoard(board, colSelection, color) {
+  let col = getCol(board, colSelection);
+  let spaceToUpdate;
+
+  for (let i = 0; i < col.length; i++) {
+    if (col[i].state === 0) {
+      spaceToUpdate = col[i];
+      break;
+    }
+  }
+
+  let x = spaceToUpdate.x;
+  let y = spaceToUpdate.y;
+  board[x][y].state = color;
+  return board;
 }
 
-function Board(x, y) {
+function Space(x, y) {
+  this.state = EMPTY;
+  this.x = x;
+  this.y = y;
+}
+
+function initBoard(x, y) {
   let xMax = x || 7;
   let yMax = y || 6;
   let board = new Array();
@@ -24,7 +57,7 @@ function Board(x, y) {
   for (let x = 0; x < xMax; x++) {
     board[x] = new Array();
     for (let y = 0; y < yMax; y++) {
-      board[x][y] = new Space();
+      board[x][y] = new Space(x, y);
     }
   }
 
