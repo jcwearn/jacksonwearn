e1ad8dc067ddb2ca0a5005d1c547359e97baea9e
Jackson Wearn <jacksonwearn@gmail.com>
2017-05-04 20:02:35 -0400

    Make board more like an object rather than a function

--- public/javascripts/connect-four.js
+++ public/javascripts/connect-four.js
@@ -10,31 +10,23 @@ function Space(x, y) {
   this.state = EMPTY;
   this.x = x;
   this.y = y;
-
-  if(y + 1 > 0)
-    this.north = y + 1;
-
-   if(y - 1 > 0)
-     this.south = y + 1;
-
-   if(x + 1 > 0)
-     this.east = y + 1;
-
-   if(y + 1 > 0)
-    this.west = y + 1;
 }
 
 function Board(x, y) {
   let xMax = x || 7;
   let yMax = y || 6;
-  let board = new Array();
+  let spaces = new Array();
 
   for (let x = 0; x < xMax; x++) {
-    board[x] = new Array();
+    spaces[x] = new Array();
     for (let y = 0; y < yMax; y++) {
-      board[x][y] = new Space(x, y);
+      spaces[x][y] = new Space(x, y);
     }
   }
 
-  return board;
+  this.spaces = spaces;
 }
+
+Board.prototype.getSpace = function(x, y) {
+  return this.spaces[x][y];
+};
