a8bc6921778cc692192d8df6cb53265c37e37d20
Jackson Wearn <jacksonwearn@gmail.com>
2017-05-05 14:39:07 -0400

    rename canvas_helper connect_four_display

---
+++ public/javascripts/connect_four_display.js
@@ -0,0 +1,15 @@
+function init() {
+  // var canvas = document.getElementById('connect-four');
+  // if (canvas.getContext){
+  //   let h = parseInt(canvas.getAttribute("height"));
+  //   let w = parseInt(canvas.getAttribute("width"));
+
+  //   let ctx = canvas.getContext('2d');
+
+  //   ctx.fillStyle = "#000000";
+  //   ctx.fillRect(0,0,w,h);
+
+  //   var game = new Game();
+  // }
+}
+window.onload = init;
