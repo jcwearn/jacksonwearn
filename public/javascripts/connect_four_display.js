158cb3ee034d7ea2cf8573308e230bbc8bae2799
Jackson Wearn <jacksonwearn@gmail.com>
2017-05-05 23:37:17 -0400

    Basic piece dropping physics

--- public/javascripts/connect_four_display.js
+++ public/javascripts/connect_four_display.js
@@ -1,15 +1,65 @@
-function init() {
-  // var canvas = document.getElementById('connect-four');
-  // if (canvas.getContext){
-  //   let h = parseInt(canvas.getAttribute("height"));
-  //   let w = parseInt(canvas.getAttribute("width"));
+var canvas = document.getElementById("connect-four"),
+    ctx = canvas.getContext("2d");
 
-  //   let ctx = canvas.getContext('2d');
+var W = 600,
+    H = 400;
 
-  //   ctx.fillStyle = "#000000";
-  //   ctx.fillRect(0,0,w,h);
+var background = {
+  draw: function() {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0,0,W,H);
+  }
+};
 
-  //   var game = new Game();
-  // }
+
+var grid = {
+  draw: function() {
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(W + 0.5, H + 0.5);
+    ctx.stroke();
+  }
+};
+
+var ball = {},
+    gravity = 0.2,
+    bounceFactor = 0.1;
+
+ball = {
+  x: W/2,
+  y: 50,
+
+  radius: 15,
+  color: "#18BC9C",
+
+  vx: 0,
+  vy: 1,
+
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+};
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, W, H);
 }
-window.onload = init;
+
+function update() {
+  clearCanvas();
+  background.draw();
+  grid.draw();
+  ball.draw();
+
+  ball.y += ball.vy;
+  ball.vy += gravity;
+  if(ball.y + ball.radius > H) {
+    ball.y = H - ball.radius;
+    ball.vy *= -bounceFactor;
+  }
+}
+
+setInterval(update, 1000/60);
