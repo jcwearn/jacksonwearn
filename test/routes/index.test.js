94fdb6bfffd26c352c2c242caf619212ad8270bb
Jackson Wearn <jacksonwearn@gmail.com>
2017-06-11 10:58:43 -0400

    Most basic test for routes

---
+++ test/routes/index.test.js
@@ -0,0 +1,15 @@
+var expect = require('expect.js');
+var proxyquire = require('proxyquire');
+
+var router = require('../../routes/index.js');
+
+describe("routes/index.js", () => {
+  it('should have a route for /', (done) => {
+    expect(router.stack[0].route.path).to.eql('/');
+    done();
+  });
+  it('should have a route for /contact', (done) => {
+    expect(router.stack[1].route.path).to.eql('/contact');
+    done();
+  });
+});
