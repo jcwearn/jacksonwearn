63739be28a5e5ca04051ad387ae1d5bb8bc9e0c2
Wearn, Jackson <jackson.wearn@turner.com>
2018-05-27 20:36:02 -0400

    fix static route for index

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const contactRouter = require('./routes/contactRouter');
 
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'index.html')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/contact', contactRouter);
+app.use('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
