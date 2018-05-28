3662eb68ed59e33932b5d75e44bb455b176e01e7
Wearn, Jackson <jackson.wearn@turner.com>
2018-05-28 13:47:18 -0400

    lint files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const contactRouter = require('./routes/contactRouter');
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,20 +19,20 @@ app.use('/', function(req, res, next) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	const err = new Error("Not Found");
-	err.status = 404;
-	next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.redirect("/error.html");
+    // render the error page
+    res.status(err.status || 500);
+    res.redirect('/error.html');
 });
 
 module.exports = app;
