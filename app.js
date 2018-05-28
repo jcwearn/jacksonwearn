97501f81c8d853b6d4f98ce7aa62760675068991
Wearn, Jackson <jackson.wearn@turner.com>
2018-05-28 13:33:24 -0400

    uncomment error handlers

--- app.js
+++ app.js
@@ -17,22 +17,22 @@ app.use('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-// 	const err = new Error("Not Found");
-// 	err.status = 404;
-// 	next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-// 	// set locals, only providing error in development
-// 	res.locals.message = err.message;
-// 	res.locals.error = req.app.get("env") === "development" ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-// 	// render the error page
-// 	res.status(err.status || 500);
-// 	res.redirect("/error.html");
-// });
+	// render the error page
+	res.status(err.status || 500);
+	res.redirect("/error.html");
+});
 
 module.exports = app;
